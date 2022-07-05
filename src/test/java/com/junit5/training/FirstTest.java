8c13cb3ebaf14286b2eb775fe1e3f2b4d9f3db3c
Sergey Starostin <adelaide.star@gmail.com>
2022-07-06 00:42:07 +0300

    Added sample

--- src/test/java/com/junit5/training/FirstTest.java
+++ src/test/java/com/junit5/training/FirstTest.java
@@ -12,27 +12,38 @@ public class FirstTest {
     @BeforeAll
     static void setUp() {
         Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 700;
+        //Configuration.holdBrowserOpen = true;
+       // Configuration.timeout = 700;
     }
 
 
     @DisplayName("проверка выдачи результатов по слову Смеситель в Максидоме")
     @Test
     void simpleTest() {
-        Selenide.open("https://www.maxidom.ru");
+        Selenide.open("https://petrovich.ru/");
+
+
+        $(By.className("header-search-input")).setValue("Смеситель");
+        $(By.className("pt-btn-fullheight___W5w5D")).click();
+
+    }
+    @Disabled
+    @DisplayName("проверка выдачи результатов по слову Смеситель в Леруа")
+    @Test
+    void simpleTestLeroy() {
+        Selenide.open("https://leroymerlin.ru");
 
 
         //$(By.className("search__input")).setValue("Смеситель");
         //$("#q").setValue("table");
         //$(".search__submit").click();
-        $(By.className("search__submit")).click();
+       // $(By.className("search__submit")).click();
 
     }
 
-    @AfterAll
-    static void close() {
-       Configuration.timeout = 700;
-       }
+   // @AfterAll
+    //static void close() {
+     // Configuration.timeout = 700;
+     // }
 
 }
